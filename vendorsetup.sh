aace3b0c4b70193cb6025f34ce047227208308cc
placiano <placiano80@gmail.com>
2016-03-18 22:36:29 +0100

    Fix device name in script
    Signed-off-by: placiano <placiano80@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,15 @@
-for device in $(cat vendor/emotion/emotion.devices)
-do
-add_lunch_combo emotion_$device-userdebug
-done
+add_lunch_combo emotion_angler-userdebug
+add_lunch_combo emotion_bacon-userdebug
+add_lunch_combo emotion_bullhead-userdebug
+add_lunch_combo emotion_condor-userdebug
+add_lunch_combo emotion_falcon-userdebug
+add_lunch_combo emotion_flo-userdebug
+add_lunch_combo emotion_hammerhead-userdebug
+add_lunch_combo emotion_hammerheadcaf-userdebug
+add_lunch_combo emotion_mako-userdebug
+add_lunch_combo emotion_m8-userdebug
+add_lunch_combo emotion_oneplus2-userdebug
+add_lunch_combo emotion_trltexx-userdebug
+add_lunch_combo emotion_trltespr-userdebug
+add_lunch_combo emotion_trltespr-userdebug
+add_lunch_combo emotion_trltetmo-userdebug
