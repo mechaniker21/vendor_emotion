3c8bbe08804e13b88c649696ffdf7567ac20d7bd
placiano <placiano80@gmail.com>
2016-04-17 18:13:38 +0200

    vendor: change version to Unofficial
    Signed-off-by: placiano <placiano80@gmail.com>

--- tools/build-emotion.sh
+++ tools/build-emotion.sh
@@ -62,7 +62,7 @@ usage() {
 # EMOTION version
 export EMOTION_VERSION_MAJOR="MM"
 export EMOTION_VERSION_MINOR="r5"
-export EMOTION_VERSION_MAINTENANCE="Official"
+export EMOTION_VERSION_MAINTENANCE="Unofficial"
 # Acceptable maintenance versions are; Stable, Official, Nightly or Unofficial
 
 
