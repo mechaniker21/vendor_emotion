eefc32e77faeea3eab8c4d070b1ca6bbfdf7f3fb
Pafcholini <nadyaivanova14@gmail.com>
2016-05-31 01:09:46 +0200

    r11 release
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- tools/build-emotion.sh
+++ tools/build-emotion.sh
@@ -61,7 +61,7 @@ usage() {
 
 # EMOTION version
 export EMOTION_VERSION_MAJOR="MM"
-export EMOTION_VERSION_MINOR="r10"
+export EMOTION_VERSION_MINOR="r11"
 export EMOTION_VERSION_MAINTENANCE="Unofficial"
 # Acceptable maintenance versions are; Stable, Official, Nightly or Unofficial
 
