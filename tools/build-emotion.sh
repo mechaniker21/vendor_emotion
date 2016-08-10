fb48388753ec919213e8ef448858bdc5ede453d0
Pafcholini <nadyaivanova14@gmail.com>
2016-08-10 21:04:06 +0200

    bump to r17
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- tools/build-emotion.sh
+++ tools/build-emotion.sh
@@ -62,7 +62,7 @@ usage() {
 
 # EMOTION version
 export EMOTION_VERSION_MAJOR="MM"
-export EMOTION_VERSION_MINOR="r16.1"
+export EMOTION_VERSION_MINOR="r17"
 export EMOTION_VERSION_MAINTENANCE="Unofficial"
 # Acceptable maintenance versions are; Stable, Official, Nightly or Unofficial
 
