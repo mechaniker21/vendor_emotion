a40c135af8f5477148dccf23b541f0ac31e84143
pafcholini <nadyaivanova14@gmail.com>
2016-10-22 17:15:32 +0200

    configs: build EmotionOTA
    Signed-off-by: pafcholini <nadyaivanova14@gmail.com>

--- configs/common.mk
+++ configs/common.mk
@@ -154,8 +154,8 @@ PRODUCT_PACKAGES += \
     AdAway \
     LatinIME \
     LatinImeDictionaryPack \
-    Stk
-#   ROMControl \
+    Stk \
+    EmotionOTA
 
 #Build EmotionOTA only if EMOTION_VERSION_MAINTENANCE isn't Unofficial
 ifneq ($(EMOTION_VERSION_MAINTENANCE),Unofficial)
