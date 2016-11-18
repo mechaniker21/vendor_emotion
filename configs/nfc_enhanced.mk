50b74ede54d745b99fe68ee786651e241b689e53
JuginKrebs <klilikddfg@hotmail.de>
2016-11-18 08:40:40 -0500

    Delete nfc_enhanced.mk

--- configs/nfc_enhanced.mk
+++
@@ -1,3 +0,0 @@
-# This device supports EMOTION enhanced NFC
-PRODUCT_COPY_FILES += \
-    vendor/emotion/configs/permissions/com.emotion.nfc.enhanced.xml:system/etc/permissions/com.emotion.nfc.enhanced.xml
