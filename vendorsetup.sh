c2ae6d6c92c1087ad3ac4670d354495f61af0725
Anonymous <klilikddfg@hotmail.de>
2016-12-04 20:01:44 +0100

    Delete Devices

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,1 @@
-add_lunch_combo emotion_angler-userdebug
-add_lunch_combo emotion_bacon-userdebug
-add_lunch_combo emotion_bullhead-userdebug
-add_lunch_combo emotion_condor-userdebug
-add_lunch_combo emotion_falcon-userdebug
-add_lunch_combo emotion_flo-userdebug
-add_lunch_combo emotion_hammerhead-userdebug
-add_lunch_combo emotion_hammerheadcaf-userdebug
-add_lunch_combo emotion_mako-userdebug
-add_lunch_combo emotion_m8-userdebug
-add_lunch_combo emotion_oneplus2-userdebug
-add_lunch_combo emotion_trltexx-userdebug
-add_lunch_combo emotion_trltespr-userdebug
-add_lunch_combo emotion_trltetmo-userdebug
-add_lunch_combo emotion_expressltexx-userdebug
 add_lunch_combo emotion_oneplus3-userdebug
